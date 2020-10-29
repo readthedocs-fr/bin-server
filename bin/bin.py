5d8143fdca95cdfc8779338472144227913ed5f1
Pilna <pilna@outlook.fr>
2020-10-29 02:07:22 +0100

    basic REST api with a few html template files

--- bin/bin.py
+++ bin/bin.py
@@ -1,5 +1,29 @@
-import bottle
+from bottle import route, template, error, static_file
+from os import path
+from bin import root
 
-@bottle.route('/')
+@route('/assets/<dir>/<filepath:path>')
+def styles(dir=None, filepath=None):
+    return static_file(path.join(dir, filepath), root=root.joinpath('assets'))
+
+@route('/', method='GET')
 def index():
-    return
+    return template('index.html')
+
+@route('/new', method='POST')
+def publish_new_snippet():
+    return 'publish_new_snippet'
+
+@route('/<snippet>', method='GET')
+@route('/<snippet>.<ext>', method='GET')
+def display_with_coloration(snippet, ext=None):
+    return 'display_with_coloration'
+
+@route('/raw/<snippet>', method='GET')
+@route('/raw/<snippet>.<ext>', method='GET')
+def display_text(snippet, ext=None):
+    return 'display_text'
+
+@error(404)
+def error404(error):
+    return "404"
