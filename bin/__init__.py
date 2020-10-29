18b05e545c3e97d2cc6eaf296363c086d1087c56
Aca <mrl.adel.sanorel@gmail.com>
2020-10-29 19:35:39 +0100

    feat: added highlight.js, new pages, update .gitignore
    Highlight.js was added with the theme Ocean, "publish" button was added to the main page, the highlight page was created with a little script to count the number of lines.

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,20 +1,2 @@
 root = __import__('pathlib').Path(__file__).resolve().parent
 __import__('bottle').TEMPLATE_PATH = [str(root / 'views')]
-
-languages_exts = {
-    'py': 'python',
-    'js': 'javascript',
-    'md': 'markdown',
-    'cs': 'csharp',
-    'sh': 'kotlin',
-    'h': 'objectivec',
-    'hpp': 'cpp',
-    'rb': 'ruby',
-    'rs': 'rust',
-    'ts': 'typescript',
-    'yml': 'yaml',
-    'txt': 'plaintext',
-    'html': 'html',
-    'css': 'css',
-    None: '',
-}
--- bin/bin.py
+++ bin/bin.py
@@ -3,6 +3,25 @@ from os import path
 from bin import root
 
 
+languages_exts = {
+    'py': 'python',
+    'js': 'javascript',
+    'md': 'markdown',
+    'cs': 'csharp',
+    'sh': 'kotlin',
+    'h': 'objectivec',
+    'hpp': 'cpp',
+    'rb': 'ruby',
+    'rs': 'rust',
+    'ts': 'typescript',
+    'yml': 'yaml',
+    'txt': 'plaintext',
+    'html': 'html',
+    'css': 'css',
+    None: '',
+}
+
+
 @bottle.route(path='/public/<dir>/<filepath:path>')
 def static_files(dir=None, filepath=None):
     return bottle.static_file(path.join(dir, filepath), root=root.joinpath('public'))
@@ -20,8 +39,11 @@ def post_new_snippet():
 
 @bottle.route(path='/<id>', method='GET')
 @bottle.route(path='/<id>.<ext>', method='GET')
-def get_highlight_snippet(id, ext='txt'):
-    return 'Highlight snippet'
+def get_highlight_snippet(id, ext='py'):
+    code = '@route(\'/raw/<snippet_id>\')\n@route(\'/raw/<snippet_id>.<ext>\', method=\'GET\')\ndef get_raw_snippet(snippet_id, ext=None):\n    return database.get(snippet_id)'
+    if code:
+        return bottle.template('highlight.html', code=code, lang=languages_exts.get(ext))
+    raise bottle.HTTPError(404, 'Snippet not found')
 
 
 @bottle.route(path='/raw/<id>', method='GET')
@@ -30,11 +52,6 @@ def get_raw_snippet(id, ext='txt'):
     return 'Raw snippet'
 
 
-@bottle.error(code=404)
-def error_handler_404(error):
-    return f'Error 404: (Page not found) {error}'
-
-
-@bottle.error(code=500)
-def error_handler_500(error):
-    return f'Error 500: (Internal server error) {error}'
+@bottle.error()
+def error_handler(error):
+    return error
