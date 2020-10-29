3b71e7fa8a49c95bc06a7c2e6b1a12db5f4071e5
Pilna <pilna@outlook.fr>
2020-10-29 15:16:43 +0100

    add correspondance extension name of language

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,2 +1,18 @@
 root = __import__("pathlib").Path(__file__).resolve().parent
 __import__('bottle').TEMPLATE_PATH = [str(root / 'views')]
+nameOfExtension = {
+    'py': 'python',
+    'js': 'javascript',
+    'md': 'markdown',
+    'cs': 'csharp',
+    'sh': 'shell',
+    'kts': 'kotlin',
+    'h': 'objectivec',
+    'hpp': 'cpp',
+    'rb': 'ruby',
+    'rs': 'rust',
+    'ts': 'typescript',
+    'yml': 'yaml',
+    'txt': 'plaintext',
+    None: "",
+}
