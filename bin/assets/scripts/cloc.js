a5a6c89efd4b240299cdf051450fe7d0dfa3d824
FuseTim <fusetim@gmx.com>
2020-10-31 13:06:17 +0100

    fix: correction of the line numbering display
    Number of lines is now retrieved by using the code element.
    Thus, we do not replicate the code and we can now safely use newline.
    
    Also fixed a typo that prevented the launch of scripts on `onload` event.

--- bin/assets/scripts/cloc.js
+++ bin/assets/scripts/cloc.js
@@ -1,5 +1,7 @@
 onloads.push(() => {
 	const linesElement = document.getElementById("cloc");
+	const bin = document.getElementById("bin");
+	const code = bin.innerText;
 	let htmlFormat = "1";
 
 	for(let numberOfLines = 2; numberOfLines <= code.split('\n').length; numberOfLines++)
