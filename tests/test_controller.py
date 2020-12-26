63f300524e13552297991d14463a512404a166cb
Mesteery <48163546+Mesteery@users.noreply.github.com>
2020-12-26 17:42:23 +0100

    fix: test accept some tags as singles pairs
    The test unit produced an error if the "option" item was not closed.
    
    Now it accepts "option" and "optgroup" as singles pairs
    if they are respectively direct children of datalist and select.
    
    Co-authored-by: Julien Castiaux <julien.castiaux@gmail.com>

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -27,28 +27,26 @@ class HTMLSanitizer(HTMLParser):
     def __init__(self, testcase):
         self.testcase = testcase
         self.pairtags = {
-            'a', 'abbr', 'acronym', 'address', 'applet', 'article',
-            'aside', 'audio', 'b', 'basefont', 'bdi', 'bdo', 'big',
-            'blockquote', 'body', 'button', 'canvas', 'caption',
-            'center', 'cite', 'code', 'colgroup', 'data', 'datalist',
-            'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl',
-            'doctype', 'dt', 'em', 'fieldset', 'figcaption', 'figure', 'font',
-            'footer', 'form', 'frame', 'frameset', 'head', 'header', 'hgroup',
-            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'html', 'i', 'iframe',
-            'ins', 'kbd', 'label', 'legend', 'li',
-            'main', 'map', 'mark', 'menu', 'menuitem', 'meter',
-            'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup',
-            'option', 'output', 'p', 'picture', 'pre', 'progress',
-            'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section',
-            'select', 'small', 'span', 'strike', 'strong', 'style',
+            'a', 'abbr', 'acronym', 'address', 'applet', 'article', 'aside',
+            'audio', 'b', 'basefont', 'bdi', 'bdo', 'big', 'blockquote',
+            'body', 'button', 'canvas', 'caption', 'center', 'cite', 'code',
+            'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn',
+            'dialog', 'dir', 'div', 'dl', 'doctype', 'dt', 'em', 'fieldset',
+            'figcaption', 'figure', 'font', 'footer', 'form', 'frame',
+            'frameset', 'head', 'header', 'hgroup', 'h1', 'h2', 'h3', 'h4',
+            'h5', 'h6', 'html', 'i', 'iframe', 'ins', 'kbd', 'label', 'legend',
+            'li', 'main', 'map', 'mark', 'menu', 'menuitem', 'meter', 'nav',
+            'noframes', 'noscript', 'object', 'ol', 'output', 'p', 'picture',
+            'pre', 'progress','q', 'rp', 'rt', 'ruby', 's', 'samp', 'script',
+            'section', 'select', 'small', 'span', 'strike', 'strong', 'style',
             'sub', 'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template',
-            'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
-            'tt', 'u', 'ul', 'var', 'video',
+            'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'tt',
+            'u', 'ul', 'var', 'video',
         }
         self.singletags = {
-            "area", "base", "br", "col", "command", "embed", "hr", "img",
-            "input", "keygen", "link", "meta", "param", "source", "track",
-            "wbr",
+            'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img',
+            'input', 'keygen', 'link', 'meta', 'param', 'source', 'track',
+            'wbr',
         }
         self.stack = []
         self.insvg = False
@@ -56,6 +54,14 @@ class HTMLSanitizer(HTMLParser):
 
     def handle_starttag(self, starttag, attrs):
         if self.stack and self.stack[-1] == 'svg':
+            return
+
+        if starttag in ('option', 'optgroup'):
+            # <option> and <optgroup> can be single or pair, no need to verify
+            # append them the the stack, we just verify their immediate parent
+            # tag is a <select> or a <datalist>.
+            self.testcase.assertTrue(self.stack[-1] in ('select', 'datalist'),
+                "%s must be direct child of select or datalist")
             return
 
         self.testcase.assertIn(starttag, self.pairtags | self.singletags, "unknown html tag")
@@ -66,10 +72,25 @@ class HTMLSanitizer(HTMLParser):
         if self.stack and self.stack[-1] == 'svg':
             return
 
+        if startendtag in ('option', 'optgroup'):
+            # <option> and <optgroup> can be single or pair, we just verify
+            # their immediate parent tag is a <select> or a <datalist>.
+            self.testcase.assertTrue(self.stack[-1] in ('select', 'datalist'),
+                "%s must be direct child of select or datalist")
+            return
+
         self.testcase.assertIn(startendtag, self.singletags)
 
     def handle_endtag(self, endtag):
         if self.stack[-1] == 'svg' and endtag != 'svg':
+            return
+
+        if endtag in ('option', 'optgroup'):
+            # <option> and <optgroup> can be single or pair, they were not
+            # added to the stack, we just verify their immediate parent is
+            # a <select> or a <datalist>
+            self.testcase.assertTrue(self.stack[-1] in ('select', 'datalist'),
+                "%s must be direct child of select or datalist")
             return
 
         starttag = self.stack.pop()
@@ -120,7 +141,8 @@ class TestController(unittest.TestCase):
         bottle.template = MagicMock()
         bottle.template.side_effect = bottle_template
 
-    def tearDownClass(self):
+    @classmethod
+    def tearDownClass(cls):
         bottle.template = bottle_template
 
     def setUp(self):
