aedbd812f2cb8a71906d29a9413c530bf70147c0
Mesteery <48163546+Mesteery@users.noreply.github.com>
2020-12-28 13:47:02 +0100

    imp: anchors
    Closes: https://github.com/readthedocs-fr/bin/issues/9.
    
    To do this, I had to completely change the structure of the code,
    I used a `table`.
    
    I also adapted the unit test to the new structure.

--- bin/highlight.py
+++ bin/highlight.py
@@ -3,25 +3,25 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 
 
-class OlLiHtmlFormatter(HtmlFormatter):
+class TableHtmlFormatter(HtmlFormatter):
     def __init__(self, **options):
         super().__init__(**options)
-        if options.get('linenos', False) == 'ol':
+        if options.get('linenos', False) == 'bin-table':
             self.linenos = 3
-            self.lineseparator = '</li><!--\n--><li>'
 
     def wrap(self, source, outfile):
         if self.linenos == 3:
-            source = self._wrap_olli(source)
-        return super().wrap(source, outfile)
+            source = self._wrap_table(source)
+        yield from source
 
-    def _wrap_olli(self, inner):
-        yield 0, '<ol><li>'
-        yield from inner
-        yield 0, '</li></ol>'
+    def _wrap_table(self, inner):
+        yield 0, '<table class="highlight"><tbody>'
+        for i, (t, l) in enumerate([*inner, (1, '')]):
+            yield t, f'<tr><td class="line-number" id=L{i + 1} value={i + 1}></td><td class="line-content">{l}</td></tr>\n'
+        yield 0, '</tbody></table>'
 
 
-_html_formatter = OlLiHtmlFormatter(wrapcode=True, linenos='ol', style='monokai')
+_html_formatter = TableHtmlFormatter(linenos='bin-table', style='monokai')
 
 
 def highlight(code, language):
