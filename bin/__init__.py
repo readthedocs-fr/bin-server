19c44e79aac53fbd9c19d6f51387c213ef2d65ed
Julien Castiaux <julien.castiaux@gmail.com>
2021-01-06 22:26:06 +0100

    add: easy way to get the wsgi entrypoint

--- bin/__init__.py
+++ bin/__init__.py
@@ -2,6 +2,7 @@ import bottle
 
 root = __import__("pathlib").Path(__file__).resolve().parent
 bottle.TEMPLATE_PATH = [str(root / 'views')]
+app = application = bottle.default_app()
 
 from . import config
 from . import utils
