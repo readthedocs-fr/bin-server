609861cf4bf3505e28899ed1268972caad17e363
Julien Castiaux <julien.castiaux@gmail.com>
2021-01-13 04:26:38 +0100

    fix: change url of shell script

--- rtdbin.sh
+++ rtdbin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test -z $1 && file=- || file=$1
 test -z $1 && lang=txt || lang="${file#*.}"
-url=$(curl -s -F file=@$file -F "lang=$lang" -w %{redirect_url} http://localhost:8012/new)
+url=$(curl -s -F file=@$file -F "lang=$lang" -w %{redirect_url} https://bin.readthedocs.fr/new)
 echo -n $url | xsel -p
 curl -s $(echo $url | sed -E 's/^(.*)\/([a-z\.]*)$/\1\/raw\/\2/') | batcat -n -l $lang
