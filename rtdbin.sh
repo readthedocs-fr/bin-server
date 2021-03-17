753b6e5b8d3512610c7ca7c4a4e9cab35f24a716
Julien Castiaux <Julien.castiaux@gmail.com>
2021-03-17 19:40:17 +0100

    Update rtdbin.sh

--- rtdbin.sh
+++ rtdbin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 test -z $1 && file=- || file=$1
 test -z $1 && lang=txt || lang="${file#*.}"
-url=$(curl -s -F file=@$file -F "lang=$lang" -w %{redirect_url} https://bin.readthedocs.fr/new)
+curl -s -F file=@$file -F "lang=$lang" -w %{redirect_url}\n https://bin.readthedocs.fr/new
