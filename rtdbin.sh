26444a045c572cf49905e3157565dcf164abdb84
Julien Castiaux <julien.castiaux@gmail.com>
2021-04-01 00:27:17 +0200

    chore(rtdbin.sh): custom user-agent

--- rtdbin.sh
+++ rtdbin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 test -z $1 && file=- || file=$1
 test -z $1 && lang=txt || lang="${file#*.}"
-curl -s -F file=@$file -F "lang=$lang" -w %{redirect_url}\n https://bin.readthedocs.fr/new
+curl_version=$(curl -V | head -n1 | cut -d ' ' -f2)
+curl -s -F file=@$file -F "lang=$lang" -w "%{redirect_url}\n" -A "curl/$curl_version (+https://github.com/readthedocs-fr/bin-server)" https://bin.readthedocs.fr/new
